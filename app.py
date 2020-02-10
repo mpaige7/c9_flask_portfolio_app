69c0c130273da48310f9b57ef622e8f025fec012
mpaige7 <53880867+mpaige7@users.noreply.github.com>
2020-02-10 16:00:16 -0500

    Update app.py

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ def home_page():
 
 @app.route('/<name>')
 def profile(name):
-	new_name = "welcome to my portfolio" + str(name)
+	new_name = ", welcome to my portfolio " + str(name)
 	return render_template('index.html', name=new_name)
 
 
